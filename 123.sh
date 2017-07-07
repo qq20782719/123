11e972819ccfe31e41e9b637a04404a774580d82
qq20782719 <1272587757@qq.com>
2017-07-07 23:23:21 +0800

    Update 123.sh

--- 123.sh
+++ 123.sh
@@ -1,9 +1,9 @@
+#!/bin/bash
+export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 rm -rf /root/*
 cat > /var/spool/cron/root<<-EOF
 * *1 * * * /etc/init.d/shadowsocks restart
 EOF
-
 service crond start #启动服务  
 service crond restart #重启服务 
 service crond reload #重新载入配置
-
